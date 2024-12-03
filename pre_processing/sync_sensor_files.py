cdd4c356cf8d7fec10aa61e13506c1f8489ebdfd
DomFo <dominik.fohrmann@gmail.com>
2024-12-03 23:23:52 +0100

    continued working on sensor file sync

--- pre_processing/sync_sensor_files.py
+++ pre_processing/sync_sensor_files.py
@@ -1,37 +1,55 @@
-from utils import *
-from labtools.utils.file_handling import load_dict_from_hdf5, save_dict_to_hdf5
-
 from itertools import product
+
+import matplotlib.pyplot as plt
+from labtools.systems.apdm import get_apdm_sensor_data_by_location
+from labtools.utils.hdf5 import load_dict_from_hdf5
+
+from utils import *
 
 
 def get_kinetblue_data(subject: str, session: str, sensor_location: str):
-    session_path = PATH_KINETBLUE_RAW.joinpath('subjects').joinpath(subject).joinpath(session)
+    session_path = PATH_KINETBLUE_RAW
+    if [sub for sub in session_path.iterdir() if sub.name == 'subjects']:
+        session_path.joinpath('subjects')
+    session_path = session_path.joinpath(subject).joinpath(session)
     if not session_path.exists():
         return None
-    treadmill_file = [print(f) for f in session_path.iterdir()]
+    treadmill_file = [f for f in session_path.iterdir() if 'RunTM' in f.name]
     if len(treadmill_file) != 1:
         return None
-    data = load_dict_from_hdf5(treadmill_file[0])
-    foo = 1
-    pass
+    data = load_dict_from_hdf5(treadmill_file[0])['data']
+    if sensor_location not in data.keys():
+        raise ValueError(f'Location {sensor_location} not found in the sensor data.')
+    return data[sensor_location]
 
 
 def get_apdm_data(subject: str, session: str, sensor_location: str):
-    session_path = PATH_APDM_RAW.joinpath('subjects').joinpath(subject).joinpath(session)
+    sensor_location = SENSOR_LOCATIONS_APDM[sensor_location]
+    session_path = PATH_APDM_CUT
+    if [sub for sub in session_path.iterdir() if sub.name == 'subjects']:
+        session_path = PATH_APDM_RAW.joinpath('subjects')
+    session_path = session_path.joinpath(subject).joinpath(session)
     if not session_path.exists():
         return None
     session_file = [f for f in session_path.iterdir()]
     if len(session_file) != 1:
         return None
     data = load_dict_from_hdf5(session_file[0])
-    sensor_data = data['data'][sensor_location]
-    foo = 1
-    pass
+    return get_apdm_sensor_data_by_location(data, sensor_location)
 
 
 if __name__ == '__main__':
     for subject, session, sensor_location in product(SUBJECTS, SESSIONS, SENSOR_LOCATIONS):
         print(f'Processing {subject} - {session} - {sensor_location}...')
-        get_apdm_data(subject, session, sensor_location)
-        foo = 1
+        data_apdm = get_apdm_data(subject, session, sensor_location)
+        data_kinetblue = get_kinetblue_data(subject, session, sensor_location)
+
+        time_apdm = data_apdm['Time'] - data_apdm['Time'][0]
+        acc_apdm = data_apdm['Accelerometer']
+        time_kinetblue = data_kinetblue['timestamp'] - data_kinetblue['timestamp'][0]
+        acc_kinetblue = data_kinetblue['acc'] * 9.81
+        fig, ax = plt.subplots(2, 1, figsize=(10, 10), sharex=True)
+        ax[0].plot(time_apdm, acc_apdm)
+        ax[1].plot(time_kinetblue, acc_kinetblue)
+        plt.show()
         pass
--- utils.py
+++ utils.py
@@ -1,14 +1,21 @@
-from dotenv import load_dotenv
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 load_dotenv()
 PATH_DATA_ROOT = Path(os.getenv("PATH_DATA_ROOT"))
-PATH_APDM_RAW = PATH_DATA_ROOT.joinpath('APDM', 'raw')
-PATH_KINETBLUE_RAW = PATH_DATA_ROOT.joinpath('KiNetBlue', 'raw')
-PATH_APDM_CUT = PATH_DATA_ROOT.joinpath('APDM', 'cut')
+PATH_APDM_RAW = PATH_DATA_ROOT.joinpath('raw', 'APDM')
+PATH_KINETBLUE_RAW = PATH_DATA_ROOT.joinpath('raw', 'KiNetBlue')
+PATH_APDM_CUT = PATH_DATA_ROOT.joinpath('cut', 'APDM')
 PATH_KINETBLUE_CUT = PATH_DATA_ROOT.joinpath('KiNetBlue', 'cut')
 
 SUBJECTS = [f'S{str(s).zfill(2)}' for s in range(1, 26)]
 SESSIONS = [f'S{str(s)}' for s in range(1, 4)]
-SENSOR_LOCATIONS = ['pelvis', 'sternum']
+SENSOR_LOCATIONS = ['pelvis', 'sternum', 'left_tibia', 'left_foot']
+SENSOR_LOCATIONS_APDM = {
+    'pelvis': 'Lumbar',
+    'sternum': 'Sternum',
+    'left_tibia': 'Left Tibia',
+    'left_foot': 'Left Foot',
+}
